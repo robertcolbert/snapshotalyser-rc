cc58d98a43d5f9bc15df70bec08200751ccb6329
Robert Colbert <robert.colbert@accenture.com>
2020-02-25 12:54:22 +0000

    Snap short error handling

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -14,6 +14,10 @@ def filter_instances(project):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -89,8 +93,13 @@ def create_snapshot(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshots(v):
+                print('  Skipping {0}, snapshot already in progress'.format(v.id))
+                continue
+
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by shotty")
+        
         print("Starting {0}...".format(i.id))
         i.start()
         i.wait_until_running()
